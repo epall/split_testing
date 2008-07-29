fe88e7e32f68ffdc395510675090ebd45616afd2
Eric Allen <ericpallen@gmail.com>
2008-07-29 14:11:24 -0700

    Simple feature enabling/disabling

--- lib/feature_helper.rb
+++ lib/feature_helper.rb
@@ -1,5 +1,7 @@
 module FeatureHelper
-  def feature
-    # TODO: implement feature method
+  def feature(name)
+    feature = SplitTesting::Feature.find(:first, :conditions => {:name => name.to_s})
+    raise "Feature '#{name}' not found" if feature.nil?
+    yield if feature.enabled?
   end
 end
--- lib/split_testing/feature.rb
+++ lib/split_testing/feature.rb
@@ -1,4 +1,10 @@
 module SplitTesting
   class Feature < ActiveRecord::Base
+    def enable
+      self.update_attribute('enabled', true)
+    end
+    def disable
+      self.update_attribute('enabled', false)
+    end
   end
 end
